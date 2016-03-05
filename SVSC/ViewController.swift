0a959e79340cd65e0403eb2f76874caf2b681c79
Nathan Taylor <nathan@onosendai.local>
2016-03-05 04:43:07 -0800

    Initial Commit

---
+++ SVSC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SVSC
+//
+//  Created by Nathan Taylor on 3/5/16.
+//  Copyright © 2016 Nathan Taylor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
